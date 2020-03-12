d0162f3c66980e63d2cf706631d84b6d9771db8c
Endy Muhardin <endy@muhardin.com>
2020-03-12 13:04:16 +0700

    commit pertama

---
+++ README.md
@@ -0,0 +1,1 @@
+# Aplikasi Switch User #
---
+++ src/main/java/com/muhardin/endy/belajar/switchuser/belajarswitchuser/BelajarSwitchuserApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.switchuser.belajarswitchuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSwitchuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSwitchuserApplication.class, args);
+	}
+
+}
